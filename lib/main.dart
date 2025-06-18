31c92a71fcf8d3e93f476b84f7e3a16fb2e67034
N4suk4 <sebastien.benoit9@gmail.com>
2025-06-18 11:29:31 +0200

    Choisir villes ok

--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,72 @@ import 'package:aquatech_meteo/services/weather_service.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Testscreen(),
+      title: 'Météo App',
+      home: TestScreen(), // ← Ton TestScreen ici !
     );
   }
 }
 
-class Testscreen extends StatelessWidget {
+class TestScreen extends StatefulWidget {
+  @override
+  _TestScreenState createState() => _TestScreenState();
+}
+
+class _TestScreenState extends State<TestScreen> {
+  TextEditingController _cityController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Test Screen'),
+        title: Text('Test Weather Service'),
       ),
-      body: Center(child: ElevatedButton(
-        onPressed: () async {
-          WeatherService weatherService = WeatherService();
-          WeatherModel weather = await weatherService.getWeatherByCity('Paris');
-          print('Weather in Paris: ${weather.temperature}°C');
-        },
-        child: Text("tester paris"),
-        )
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _cityController,
+              decoration: InputDecoration(
+                labelText: 'Enter city name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed:() async{
+                String cityName = _cityController.text;
+
+                WeatherService weatherService = WeatherService();
+                WeatherModel weather = await weatherService.getWeatherByCity(cityName);
+
+                print('Weather in $cityName: ${weather.temperature}°C, '
+                      '${weather.apparentTemperature}°C apparent, '
+                      '${weather.relativeHumidity}% humidity, '
+                      '${weather.windSpeed} m/s wind, '
+                      '${weather.precipitation} mm precipitation, '
+                      '${weather.cloudCover}% cloud cover'
+                      );
+              },
+              child: Text('Get Weather'),
+            )
+          ],
+        ),
       )
     );
   }
 }
+
